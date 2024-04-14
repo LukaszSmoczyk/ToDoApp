37863979976828153e1a824bca1c0c136dadc5e1
lukasz.smoczyk <lukaszsmoczyk@gmail.com>
2024-04-14 16:29:29 +0200

    Rename

---
+++ Test/Controllers/ToDoController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoController : Controller
+    {
+    }
+}
